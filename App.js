ed2bd6a00a17ce1cdaf91752d10657aad8c2dd4b
rjvignesh <rj.vignesh12@gmail.com>
2023-01-20 13:58:26 +0530

    react native mobile app

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import WelcomeScreen from './screens/WelcomeScreen';
+import AuthScreen from './screens/AuthScreen'
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name="Welcome" component={WelcomeScreen}/>
+      <Tab.Screen name="Login" component={AuthScreen}/>
+    </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
